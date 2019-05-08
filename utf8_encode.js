f655dbdde8c95a6ad51efac484baf15574d35bee
asilinwei <1021069119@qq.com>
2019-05-08 09:44:53 +0800

    init

---
+++ utf8_encode.js
@@ -0,0 +1,26 @@
+function utf8Encode(text) {
+  "use strict";
+  
+  if (text === undefined) {
+    return '';
+  }
+  var chars = String(text).match(/[\s\S]/gu) || [],
+      length = chars.length,
+      fromCodePoint = String.fromCodePoint,
+      result = [];
+
+  for (var i = 0; i < length; i += 1) {
+    var code = chars[i].codePointAt(0);
+    if (code <= 0x007f) {
+      result.push(fromCodePoint(code));
+    } else if (code <= 0x07ff) {
+      result.push(fromCodePoint(0xc0 | (code >> 6)), fromCodePoint(0x80 | (code & 0x3f)));
+    } else if (code <= 0xffff) {
+      result.push(fromCodePoint(0xe0 | (code >> 12)), fromCodePoint(0x80 | ((code >> 6) & 0x3f)), fromCodePoint(0x80 | (code & 0x3f)));
+    } else {
+      result.push(fromCodePoint(0xf0 | (code >> 18)), fromCodePoint(0x80 | ((code >> 12) & 0x3f)), fromCodePoint(0x80 | ((code >> 6) & 0x3f)), fromCodePoint(0x80 | (code & 0x3f)));
+    }
+  } 
+  return result.join('');   
+}
+
